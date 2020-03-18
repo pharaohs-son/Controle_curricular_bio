8c07c239e22939cdcb329905630d0406dee7ca14
alissonws <48305260+alissonws@users.noreply.github.com>
2020-03-18 13:53:12 -0300

    Tabs e botões de seleção entram no armazenamento

--- scripts.js
+++ scripts.js
@@ -154,122 +154,8 @@ function dark() {
 ////////////////////////////////////////////////
 
 
-const bach_disciplinas = ["BIO7003","BIO7240","CFS7001","ECZ7011","ECZ7021","MIP7011","MOR7001","QMC5235","BEG7012","BQA7002","CFS7002","ECZ7012","MTM3100","BEG7013","BEG7225","BOT7013", "ECZ7013","INE7003","MIP7013","MOR7003","BEG7024","BEG7034","BIO7011","BOT7014","ECZ7024","MEN7004","MIP7003","BEG7025","BEG7227","BIO7012","BOT7015","ECZ7015","ECZ7025","MIP7035","CFS7006","ECZ7016","ECZ7036","DGL7006","BEG7017","BEG7226","BIO7004","BIO7013","BOT7017","ECZ7050","FIL7007","DGL7007","BIO7006","BIO7015","ECZ7028","ECZ7038","FMC7008","BIO7009","BIO7016"];
 
 
-const lic_disciplinas = ["LBIO7003","LBIO7240","LCFS7001","LECZ7011","LECZ7021","LMIP7011","LMOR7001","LQMC5235","LBEG7012","LBQA7002","LCFS7002","LECZ7012","LMTM3100","LBEG7013","LBEG7225","LBOT7013", "LECZ7013","LINE7003","LMIP7013","LMOR7003","LBEG7024","LBEG7034","LBIO7011","LBOT7014","LECZ7024","LMEN7004","LMIP7003", "LBEG7025","LBEG7227","LBIO7012","LBOT7015","LECZ7015","LECZ7025","EED5331","LMIP7035","PSI5137","LCFS7006","LECZ7016","MEN5601","LECZ7036","LDGL7006","LBIO7004","LBIO7013","LBOT7017","LECZ7050","LFIL7007","LDGL7007","LBIO7015","LBIO7016", "MEN7009", "MEN7341", "MEN7008", "LSB7904", "EED5186", "EED5185", "LBIO7009", "MEN7010"];
-
-const disciplinas = bach_disciplinas.concat(lic_disciplinas);
-
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("Loading storage");
-    //Contando listas
-    var stored = localStorage;
-    
-    for (let i = 0; i< stored.length; i++){
-        let stored_key = localStorage.getItem(stored.key(i));
-        try {
-            let list_item = JSON.parse(stored_key);
-            newElement(list_item.itemClass, list_item.itemName,list_item.itemValue,force_new=true);
-        } catch (e) {
-            continue;
-        }
-    }
-    setCloseBtnFunc();
-    
-    
-    /*Set status of each subject gotten from Storage*/
-    for (var i = 0; i < disciplinas.length; i++){
-        if (localStorage.getItem(disciplinas[i]) == "checked"){
-        document.getElementById(disciplinas[i]).disabled = false;
-        document.getElementById(disciplinas[i]).checked = true;
-    } else if (localStorage.getItem(disciplinas[i]) == "checked_enabled") {
-        document.getElementById(disciplinas[i]).disabled = false;
-        document.getElementById(disciplinas[i]).checked = false;
-    }else if (localStorage.getItem(disciplinas[i]) == "disabled"){
-        document.getElementById(disciplinas[i]).checked = false;
-        document.getElementById(disciplinas[i]).disabled = true;
-    }
-    }
-    
-     /*Activate all buttons based on storage*/
-     for (var k = 0; k < id_all.length; k++){
-       if (localStorage.getItem(id_all[k]) == "on"){
-         document.getElementById(id_all[k]).checked = true;
-       } else {
-         document.getElementById(id_all[k]).checked = false;
-       }
-     }
-	
-	
-    /*Activate dark mode based on storage*/
-    if (localStorage.getItem("theme") == "dark"){
-      document.getElementById("darkmode").checked = true;
-      dark();
-    } else {
-      document.getElementById("darkmode").checked = false;
-    }
-    
-    totalIt();
-    totalIt_lic();
-	
-}, false);
-
-
-
-function checkboxStorage(){
-    //Reiniciando o armazenamento e refazendo tudo
-    localStorage.clear();
-
-    //Computando as checkbox marcadas
-    for (var i = 0; i < disciplinas.length; i++){
-        if (document.getElementById(disciplinas[i]).disabled === false && document.getElementById(disciplinas[i]).checked === true){
-            localStorage.setItem(disciplinas[i], "checked");
-        } else if (document.getElementById(disciplinas[i]).disabled === false) {
-            localStorage.setItem(disciplinas[i],"checked_enabled");
-        }else{
-            localStorage.setItem(disciplinas[i],"disabled");
-        }
-        }
-
-    //Computando as lista de horas complementares
-    const lists = ["op_list","ae_list","acc_list","op_list_lic","ae_list_lic","acc_list_lic"];
-
-    for (let list = 0; list < lists.length;list++){
-        var input = document.getElementsByClassName(lists[list]); //Todos os items da lista
-        for (let item = 0; item < input.length; item++) {
-            let json_parse = JSON.stringify({
-            itemClass: lists[list],
-            itemName: input[item].id,
-            itemValue: input[item].value}) ;
-            localStorage.setItem(input[item].id,json_parse);
-    }
-    }
-    totalIt();
-    totalIt_lic();
-}
-
-function themeStorage(id){
-  var dark = document.getElementById(id);
-  if (dark.checked === true){
-    localStorage.setItem("theme", "dark");
-  }else {
-    localStorage.setItem("theme", "light");
-  }
-}
-
-
-
-function allStorage(){
-  for (var i = 0; i < id_all.length; i++){
-    var nome = document.getElementById(id_all[i]);
-    if (nome.checked === true){
-      localStorage.setItem(id_all[i], "on");
-    } else {
-      localStorage.setItem(id_all[i], "off");
-    }
-  }
-} 
 
 
 /////////////////////////////////////////////////////
@@ -329,8 +215,8 @@ function off_all_button(allkey, alloff){
 ////////////////////////////////////////////////
 ////////////////GERENCIAMENTO DE TABS///////////
 ////////////////////////////////////////////////
-function openTab(evt, tabName) {
-  //Dechalando variáveis
+function openTab(evt) {
+  //Declarando variáveis
   var i, tabcontent, tablinks;
 
   //Esconder todos os elementos com class="tab_content"
@@ -349,9 +235,9 @@ function openTab(evt, tabName) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "tab_button");
   }*/
-
+    
   //Mostrar a tab selecionada, aplicar a cor de botão ativo e adicionar uma classe "active" no botão que abriu a tab
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
+  document.getElementById(evt.target.id.replace("_btn","")).style.display = "block";
+    localStorage.setItem("active_tab",evt.target.id);
   evt.currentTarget.style.background = " #e54e43";
 }
